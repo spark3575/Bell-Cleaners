d390c1e53f476d2045f429c41d0cdf0af81bfdf4
Shin Park <shinpark@shins-mac-mini.local>
2017-07-06 00:03:13 -0500

    .

---
+++ Bell Cleaners/Order.swift
@@ -0,0 +1,70 @@
+//
+//  Order.swift
+//  Bell Cleaners
+//
+//  Created by Shin Park on 7/5/17.
+//  Copyright © 2017 shinparkdev. All rights reserved.
+//
+
+import Foundation
+
+class Order {
+    
+    private var _dateTime: String!
+    private var _itemsCount: String!
+    private var _orderID: String!
+    private var _orderNumber: String!
+    private var _orderStatus: String!
+    private var _orderTotal: String!
+    
+    var dateTime: String {
+        return _dateTime
+    }
+    
+    var itemsCount: String {
+        return _itemsCount
+    }
+    
+    var orderID: String {
+        return _orderID
+    }
+    
+    var orderNumber: String {
+        return _orderNumber
+    }
+    
+    var orderStatus: String {
+        return _orderStatus
+    }
+    
+    var orderTotal: String {
+        return _orderTotal
+    }
+    
+    init(dateTime: String, itemsCount: String, orderNumber: String, orderStatus: String, orderTotal: String) {
+        self._dateTime = dateTime
+        self._itemsCount = itemsCount
+        self._orderNumber = orderNumber
+        self._orderStatus = orderStatus
+        self._orderTotal = orderTotal
+    }
+    
+    init(orderID: String, orderData: [String: AnyObject]) {
+        self._orderID = orderID
+        if let dateTime = orderData["dateTime"] as? String {
+            self._dateTime = dateTime
+        }
+        if let itemsCount = orderData["itemsCount"] as? String {
+            self._itemsCount = itemsCount
+        }
+        if let orderNumber = orderData["orderNumber"] as? String {
+            self._orderNumber = orderNumber
+        }
+        if let orderStatus = orderData["orderStatus"] as? String {
+            self._orderStatus = orderStatus
+        }
+        if let orderTotal = orderData["orderTotal"] as? String {
+            self._orderTotal = orderTotal
+        }
+    }
+}
