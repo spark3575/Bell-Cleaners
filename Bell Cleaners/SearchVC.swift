22b92505d274788418892f81e62abfbc395c455a
Shin Park <shinpark@shins-mac-mini.frontierlocal.net>
2018-06-01 17:34:38 -0500

    Commit before xcode 9.4

---
+++ Bell Cleaners/SearchVC.swift
@@ -0,0 +1,17 @@
+//
+//  SearchVC.swift
+//  Bell Cleaners
+//
+//  Created by Shin Park on 12/4/17.
+//  Copyright © 2017 shinparkdev. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class SearchVC: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchResultsUpdating {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
